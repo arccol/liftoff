8b49c91c8dbf3bfb2f7301725855172e457c9b6e
arccol <20colvey.a@isaacnewtonsixthform.co.uk>
2026-06-21 19:55:57 +0100

    scene2d buttons

--- core/src/main/java/io/github/some_example_name/Main.java
+++ core/src/main/java/io/github/some_example_name/Main.java
@@ -7,6 +7,9 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.*;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import jdk.javadoc.internal.doclets.formats.html.Table;
@@ -20,26 +23,138 @@ public class Main extends ApplicationAdapter {
     ShapeRenderer sr;
     Vector2 mousePos;
     private Stage stage;
+    private Skin skin;
+    enum Screen{
+        HOME,
+        SETTINGS,
+        GAME,
+        CUSTOMISE
+    }
+    private Screen currentScreen;
+
 
     // https://libgdx.com/wiki/graphics/2d/scene2d/scene2d-ui#stage-setup
 
     @Override
     public void create() {
-        mousePos = new Vector2();
         sr = new ShapeRenderer();
         buttons = new ArrayList<>();
         planets = new ArrayList<>();
         player = new Player(0,0,10);
         stage = new Stage(new ScreenViewport());
+        skin = new Skin(Gdx.files.internal("uiskin.json"));
         Gdx.input.setInputProcessor(stage);
+        changeScreen(Screen.HOME);
+    }
+
+    public void changeScreen(Screen screen){
+        currentScreen = screen;
+        stage.clear();
+        switch(screen){
+            case HOME:
+                createHomeScreen();
+                break;
+            case SETTINGS:
+                createSettingsScreen();
+                break;
+            case GAME:
+                createGameScreen();
+                break;
+            case CUSTOMISE:
+                createCustomiseScreen();
+                break;
+        }
+    }
+
+    private void createHomeScreen(){
+        TextButton play = new TextButton("Play", skin);
+        play.setPosition(565,350);
+        play.setSize(150,80);
+        play.addListener(new ClickListener() {
+
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                changeScreen(Screen.GAME);
+            }
+        });
+
+        TextButton settings = new TextButton("Settings", skin);
+        settings.setPosition(290, 350);
+        settings.setSize(150,80);
+        settings.addListener(new ClickListener() {
+
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                changeScreen(Screen.SETTINGS);
+            }
+        });
+
+        TextButton customise = new TextButton("Customise", skin);
+        customise.setPosition(840, 350);
+        customise.setSize(150,80);
+        customise.addListener(new ClickListener() {
+
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                changeScreen(Screen.CUSTOMISE);
+            }
+        });
+
+        stage.addActor(play);
+        stage.addActor(settings);
+        stage.addActor(customise);
+    }
+
+    private void createSettingsScreen(){
+
+        TextButton back = new TextButton("Back", skin);
+        back.setPosition(0, 900);
+        back.setSize(100, 60);
+        back.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                changeScreen(Screen.HOME);
+            }
+        });
+
+        stage.addActor(back);
+    }
+
+    private void createCustomiseScreen(){
+
+        TextButton back = new TextButton("Back", skin);
+        back.setPosition(0, 900);
+        back.setSize(100, 60);
+        back.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                changeScreen(Screen.HOME);
+            }
+        });
+
+        stage.addActor(back);
+    }
+
+    private void createGameScreen() {
+
+        // game
+
+        TextButton back = new TextButton("Back", skin);
+        back.setPosition(0, 900);
+        back.setSize(100, 60);
+        back.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                changeScreen(Screen.HOME);
+            }
+        });
+
+        stage.addActor(back);
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        mousePos.x = Gdx.input.getX();
-        mousePos.y = 960-Gdx.input.getY();
-
         float delta = Gdx.graphics.getDeltaTime();
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.act(delta);
@@ -49,6 +164,7 @@ public class Main extends ApplicationAdapter {
     @Override
     public void dispose() {
         stage.dispose();
+        skin.dispose();
     }
 
     public void resize(int width, int height){
