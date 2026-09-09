47a4d6a6019acdf406f15a074ff0e48705556755
arccol <20colvey.a@isaacnewtonsixthform.co.uk>
2026-09-09 12:54:23 +0100

    add goal asap

--- core/src/main/java/io/github/some_example_name/Trajectory.java
+++ core/src/main/java/io/github/some_example_name/Trajectory.java
@@ -23,7 +23,7 @@ public class Trajectory {
         this.planets = planets;
         this.mouseForce = mouseForce;
         this.rad = rad;
-        leng = 500000;
+        leng = 5000;
         markers = new Vector2[leng];
     }
 
